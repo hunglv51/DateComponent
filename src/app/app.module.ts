561800b6207e9ecc89a834e6c9a18a4a4fa5c969
Hung Le Viet <le.viet.hung@kloon.vn>
2018-08-17 18:03:00 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+ 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+ 
+import { NgCalendarModule  } from 'ionic2-calendar';
+ 
 @NgModule({
   declarations: [
     MyApp,
     HomePage
   ],
   imports: [
+    NgCalendarModule,
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,67 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController, ModalController, AlertController } from 'ionic-angular';
+import * as moment from 'moment';
+ 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  eventSource = [];
+  timeRange: string;
+  selectedDay = new Date();
+  weekNumber: number;
+  calendar = {
+    mode: 'month',
+    currentDate: new Date()
+  };
+  
+  constructor(public navCtrl: NavController, private modalCtrl: ModalController, private alertCtrl: AlertController) { 
+    
   }
+ 
+  addEvent() {
+    let modal = this.modalCtrl.create('EventModalPage', {selectedDay: this.selectedDay});
+    modal.present();
+    modal.onDidDismiss(data => {
+      if (data) {
+        let eventData = data;
+ 
+        eventData.startTime = new Date(data.startTime);
+        eventData.endTime = new Date(data.endTime);
+ 
+        let events = this.eventSource;
+        events.push(eventData);
+        this.eventSource = [];
+        setTimeout(() => {
+          this.eventSource = events;
+        });
+      }
+    });
+  }
+ modifyWeek(weeksRange){
+    this.calendar.currentDate.setDate(this.calendar.currentDate.getDate() + weeksRange * 7);
+    this.onViewTitleChanged(null);
+ }
 
+  onViewTitleChanged(title) {
+    this.weekNumber = this.calendar.currentDate.getWeekNumber();
+    this.timeRange = this.calendar.currentDate.toLocaleDateString();
+  }
+ 
+  onEventSelected(event) {
+    let start = moment(event.startTime).format('LLLL');
+    let end = moment(event.endTime).format('LLLL');
+    
+    let alert = this.alertCtrl.create({
+      title: '' + event.title,
+      subTitle: 'From: ' + start + '<br>To: ' + end,
+      buttons: ['OK']
+    })
+    alert.present();
+  }
+ 
+  onTimeSelected(ev) {
+    this.selectedDay = ev.selectedTime;
+  }
 }
