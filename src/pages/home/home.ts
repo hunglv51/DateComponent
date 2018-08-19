3c0527b11dd9f66440834c91cd38b5d564029ed8
hunglv51 <hung561992@gmail.com>
2018-08-19 20:34:58 +0700

    Add button and div

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,58 +1,82 @@
 import { Component } from '@angular/core';
 import { NavController, ModalController, AlertController } from 'ionic-angular';
 import * as moment from 'moment';
- 
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  eventSource = [];
-  timeRange: string;
-  selectedDay = new Date();
+  // eventSource = [];
+  // timeRange: string;
+  showCalendar: boolean = true;
+  selectedDate = new Date();
+  beginDate: string;
+  endDate: string;
   weekNumber: number;
   calendar = {
     mode: 'month',
-    currentDate: new Date()
+    currentDate: this.selectedDate
   };
-  
-  constructor(public navCtrl: NavController, private modalCtrl: ModalController, private alertCtrl: AlertController) { 
-    
+
+  constructor(public navCtrl: NavController, private modalCtrl: ModalController, private alertCtrl: AlertController) {
+    this.weekNumber = moment(this.selectedDate).isoWeek();
+    this.setDateBeginEnd();
   }
- 
-  addEvent() {
-    let modal = this.modalCtrl.create('EventModalPage', {selectedDay: this.selectedDay});
-    modal.present();
-    modal.onDidDismiss(data => {
-      if (data) {
-        let eventData = data;
- 
-        eventData.startTime = new Date(data.startTime);
-        eventData.endTime = new Date(data.endTime);
- 
-        let events = this.eventSource;
-        events.push(eventData);
-        this.eventSource = [];
-        setTimeout(() => {
-          this.eventSource = events;
-        });
-      }
-    });
+
+  // addEvent() {
+  //   let modal = this.modalCtrl.create('EventModalPage', {selectedDate: this.selectedDate});
+  //   modal.present();
+  //   modal.onDidDismiss(data => {
+  //     if (data) {
+  //       let eventData = data;
+
+  //       eventData.startTime = new Date(data.startTime);
+  //       eventData.endTime = new Date(data.endTime);
+
+  //       let events = this.eventSource;
+  //       events.push(eventData);
+  //       this.eventSource = [];
+  //       setTimeout(() => {
+  //         this.eventSource = events;
+  //       });
+  //     }
+  //   });
+  // }
+  setDateBeginEnd() {
+    var _beginDate = new Date(this.selectedDate);
+    var _endDate = new Date(this.selectedDate);
+    _beginDate.setDate(- this.selectedDate.getUTCDay() + this.selectedDate.getDate());
+    _endDate.setDate(6 - this.selectedDate.getUTCDay() + this.selectedDate.getDate());
+    this.beginDate = moment(_beginDate).format('D.M.YYYY');
+    this.endDate = moment(_endDate).format('D.M.YYYY');
   }
- modifyWeek(weeksRange){
+
+  onViewDidLoad() {
+
+  }
+
+  modifyWeek(weeksRange) {
     this.calendar.currentDate.setDate(this.calendar.currentDate.getDate() + weeksRange * 7);
-    this.onViewTitleChanged(null);
- }
+    // this.onViewTitleChanged(null);
+    this.weekNumber = moment(this.selectedDate).isoWeek();
+    this.setDateBeginEnd();
+  }
+
+  toggleCalendar() {
+    console.log("toggle");
+    this.showCalendar = (this.showCalendar) ? false : true;
+  }
 
   onViewTitleChanged(title) {
-    this.weekNumber = this.calendar.currentDate.getWeekNumber();
-    this.timeRange = this.calendar.currentDate.toLocaleDateString();
+
+    // this.timeRange = this.calendar.currentDate.toLocaleDateString();
   }
- 
+
   onEventSelected(event) {
     let start = moment(event.startTime).format('LLLL');
     let end = moment(event.endTime).format('LLLL');
-    
+
     let alert = this.alertCtrl.create({
       title: '' + event.title,
       subTitle: 'From: ' + start + '<br>To: ' + end,
@@ -60,8 +84,8 @@ export class HomePage {
     })
     alert.present();
   }
- 
+
   onTimeSelected(ev) {
-    this.selectedDay = ev.selectedTime;
+    this.selectedDate = ev.selectedTime;
   }
 }
